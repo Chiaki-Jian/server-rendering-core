fab9497ffb16414146d372d9b995cd2187cd4af1
Cheng Jian <chiaki0613@163.com>
2022-08-09 13:52:01 +0800

    config: 配置服务端与客户端

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <p v-if="switch">hello SSR</p>
+    <button @click="handleClick">click</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      switch: false
+    }
+  },
+  methods: {
+    handleClick() {
+      this.switch = !this.switch
+    }
+  }
+
+}
+</script>
+
+<style>
+#app {
+  width: 100%;
+  text-align: center;
+  background-color: antiquewhite;
+}
+</style>
